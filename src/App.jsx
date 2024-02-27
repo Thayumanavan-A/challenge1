5b17a3b74ec8335fbe98927aa5fbacfd134ceea6
Thayumanavan Arulnambi <thayumanavan06@gmail.com>
2024-02-27 17:30:36 +0530

    Initial Dynamic Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import React from 'react'
 import { useState } from 'react'
 import Rating from "./components/Rating";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedRating,SetSelectedRating] = React.useState();
+  const [ratingSubmit, SetRatingSubmit ] = React.useState(false);
+  const arr = [1,2,3,4,5]
+
+  const handleClick = (num)=> {
+    SetSelectedRating(num)
+  }
+  const handleSubmit = ()=> {
+    SetRatingSubmit(!ratingSubmit)
+  }
+ 
+  console.log(selectedRating)
+  console.log(ratingSubmit)
 
   return (
     <>
-      <Rating/>
+      <Rating 
+          arr={arr}
+          selectedRating={selectedRating}
+          handleClick={handleClick}
+          handleSubmit={handleSubmit}
+          />
+          
     </>
   )
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function Rating() {
+function Rating(props) {
+  const rating = props.arr.map( num => <span key={num} className='w-10 h-10 bg-gray-600 rounded-3xl flex justify-evenly items-center cursor-pointer' onClick={() => props.handleClick(num)}><p>{num}</p></span> )
   return (
     <div className='flex justify-center items-center h-screen'>
         <div className='w-1/5 h-96 bg-gray-700 rounded-2xl p-6 flex  flex-col justify-around'>
@@ -8,13 +9,9 @@ function Rating() {
             <h1 className='text-3xl  font-semibold  text-white'>How did we do ?</h1>
             <p className=' text-slate-400'>Please let us know how we did with your support request.All feedback is appreciated to help us improve our offering</p>
             <div className='flex justify-around pt-4  text-slate-400'>
-                <span className='w-10 h-10 bg-gray-600 rounded-3xl flex justify-evenly items-center'><p>1</p></span>
-                <span className='w-10 h-10 bg-gray-600 rounded-3xl flex justify-evenly items-center'><p>2</p></span>
-                <span className='w-10 h-10 bg-gray-600 rounded-3xl flex justify-evenly items-center'><p>3</p></span>
-                <span className='w-10 h-10 bg-gray-600 rounded-3xl flex justify-evenly items-center'><p>4</p></span>
-                <span className='w-10 h-10 bg-gray-600 rounded-3xl flex justify-evenly items-center'><p>5</p></span>
+              {rating}
             </div>
-            <button className='py-2 px-4 mt-4 bg-orange-500 rounded-full w-full text-white'>SUBMIT</button>
+            <button className='py-2 px-4 mt-4 bg-orange-500 rounded-full w-full text-white' onClick={props.handleSubmit}>SUBMIT</button>
         </div>
     </div>
   )
